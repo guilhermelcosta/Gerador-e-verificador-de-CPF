ee0c3b24ccf44c1f1d513f0d02a3c4118682b4e8
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-01-20 20:11:53 -0300

    Upload de gerador de CPF

--- main.js
+++ main.js
@@ -10,23 +10,56 @@ ValidaCPF.prototype.valida = function () {
   if (this.CPFLimpo.length !== 11) return false;
   if (this.ehSequencia()) return false;
   const CPFParcial = this.CPFLimpo.slice(0, 9);
-  const digito01 = this.criaDigito(CPFParcial);
-  const digito02 = this.criaDigito(CPFParcial + digito01);
+  const digito01 = criaDigito(CPFParcial);
+  const digito02 = criaDigito(CPFParcial + digito01);
   const novoCPF = CPFParcial + digito01 + digito02;
   return novoCPF === this.CPFLimpo;
 };
 
-ValidaCPF.prototype.criaDigito = function (CPFParcial) {
+function criaDigito(CPFParcial) {
   const CPFArray = Array.from(CPFParcial);
   let regressivo = CPFArray.length + 1;
   const total = CPFArray.reduce((ac, num) => (ac += Number(num) * regressivo--), 0);
   const digito = 11 - (total % 11);
   return digito > 9 ? "0" : String(digito);
-};
+}
 
 ValidaCPF.prototype.ehSequencia = function () {
   return (sequencia = this.CPFLimpo[0].repeat(this.CPFLimpo.length) === this.CPFLimpo);
 };
 
-const CPF = new ValidaCPF("705.484.450-52");
-console.log(CPF.valida() ? "CPF Válido!" : "CPF Inválido!");
+function CriaCPF() {
+  const parte01 = String(geraNumeroAleatorio());
+  const parte02 = String(geraNumeroAleatorio());
+  const parte03 = String(geraNumeroAleatorio());
+  const CPFParcial = parte01 + parte02 + parte03;
+  const digito01 = criaDigito(CPFParcial);
+  const digito02 = criaDigito(CPFParcial + digito01);
+  return `${parte01}.${parte03}.${parte03}-${digito01}${digito02}`;
+}
+
+function geraNumeroAleatorio() {
+  const num = Math.floor(Math.random() * (999 - 1) + 1);
+  return num < 100 ? `0${num}` : num;
+}
+
+// *-------------------------------------------------------- 
+// *Caso queia gerar um número de CPF aleatório, novo.
+// *--------------------------------------------------------
+let CPF = CriaCPF();
+let CPFExistente = new ValidaCPF(CPF);
+
+while (!CPFExistente.valida()) {
+  CPF = CriaCPF();
+  CPFExistente = new ValidaCPF(CPF);
+}
+
+// *-------------------------------------------------------- 
+// *Caso queria verificar um número de CPF já existente, 
+// *basta desconsiderar as linhas 49 a 55, e apenas
+// *atribuir um valor arbitrário a CPFExistente.
+// *Conforme exemplo abaixo.
+// *--------------------------------------------------------
+// const CPFExistente = new ValidaCPF("813.286.286-47");
+
+console.log(CPFExistente.valida() ? `${CPF} - CPF Válido!` : `${CPF} - CPF Inválido!`);
